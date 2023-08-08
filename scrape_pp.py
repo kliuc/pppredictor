07852bc21c679c86cb41a3b8867652698a3d14c4
kliuc <kliuc@ucla.edu>
2023-08-08 14:53:31 -0700

    add time delay to scrape_pp.py

--- scrape_pp.py
+++ scrape_pp.py
@@ -2,31 +2,31 @@ import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 
 
 options = uc.ChromeOptions()
-# options.add_argument('--headless')\
+# options.add_argument('--headless')
 driver = uc.Chrome(options=options)
 driver.maximize_window()
 wait = WebDriverWait(driver, 10)
 
 # load page and close popup
 driver.get('https://app.prizepicks.com/')
-element = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div[3]/div/div/div[3]/button')))
-element.click()
+popup = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div[3]/div/div/div[3]/button')))
+popup.click()
 
 # get all available sports leagues
 leagues = driver.find_element(By.XPATH, '//*[@id="board"]/div[1]/div/div').find_elements(By.XPATH, '*')
 leagues_dict = {league.text: league for league in leagues}
-print(leagues_dict.keys())
 
 # click on MLB
 leagues_dict['MLB'].click()
 
 # get all stats
-element = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'stat-container')))
-print(element.text)
-mlb_stats = element.find_elements(By.XPATH, '*')
+time.sleep(1)
+stat_container = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "stat-container")))
+mlb_stats = stat_container.find_elements(By.XPATH, '*')
 mlb_stats_dict = {stat.text: stat for stat in mlb_stats}
 
 # click on Pitcher Strikeouts
